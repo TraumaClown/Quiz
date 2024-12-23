df098944d451cb4e5e23bfc6ba83545cf9ca4c62
harmez7 <hr.shafiei.dev@gmail.com>
2024-12-23 04:07:32 +0330

    update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,9 @@
 const NotFound: React.FC = () => {
-  return <center className="text-5xl">404 | Not Found</center>
+  return (
+    <div className="w-full flex flex-col gap-y-8 justify-center items-center text-white">
+      <div className="text-5xl">404 | Not Found</div>
+    </div>
+  )
 }
 
 export default NotFound
